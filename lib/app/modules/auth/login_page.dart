8476b6ed43249582f7d0698a7054dd3415536a60
VictorPimentaTI <victorpimenta.ti@gmail.com>
2022-02-05 18:00:30 -0300

    Corrigido error overflow no botão entrar

--- lib/app/modules/auth/login_page.dart
+++ lib/app/modules/auth/login_page.dart
@@ -3,7 +3,7 @@ import 'package:get/get.dart';
 import 'package:vakinha_burger_mobile/app/core/ui/vakinha_ui.dart';
 import 'package:vakinha_burger_mobile/app/core/widgets/vakinha_appbar.dart';
 import 'package:vakinha_burger_mobile/app/core/widgets/vakinha_button.dart';
-import 'package:vakinha_burger_mobile/app/core/widgets/vakinha_textformfiled.dart';
+import 'package:vakinha_burger_mobile/app/core/widgets/vakinha_textformfield.dart';
 
 class LoginPage extends StatelessWidget {
 
@@ -18,63 +18,65 @@ class LoginPage extends StatelessWidget {
           backgroundColor: Colors.white,
           body: LayoutBuilder(
             builder: (_, constrainsts){
-              return ConstrainedBox(
-                constraints: BoxConstraints(
-                  minHeight: constrainsts.maxHeight,
-                ),
-                child: IntrinsicHeight(
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Form(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Login',
-                            style: context.textTheme.headline6?.copyWith(
-                              fontWeight: FontWeight.bold,
-                              color: context.theme.primaryColorDark
+              return SingleChildScrollView(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(
+                    minHeight: constrainsts.maxHeight,
+                  ),
+                  child: IntrinsicHeight(
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Form(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Login',
+                              style: context.textTheme.headline6?.copyWith(
+                                fontWeight: FontWeight.bold,
+                                color: context.theme.primaryColorDark
+                              ),
                             ),
-                          ),
-                          const SizedBox(
-                             height: 30,
-                          ),
-                          const VakinhaTextformfiled(label: 'E-mail'),
-                          const SizedBox(
-                             height: 30,
-                          ),
-                          const VakinhaTextformfiled(label: 'Senha'),
-                          const SizedBox(
-                             height: 50,
-                          ),
-                          Center(
-                            child: VakinhaButton(
-                              width: context.width,
-                              label: 'Entrar',
-                              onPressed: () {}
+                            const SizedBox(
+                               height: 30,
+                            ),
+                            const VakinhaTextformfield(label: 'E-mail'),
+                            const SizedBox(
+                               height: 30,
+                            ),
+                            const VakinhaTextformfield(label: 'Senha'),
+                            const SizedBox(
+                               height: 50,
+                            ),
+                            Center(
+                              child: VakinhaButton(
+                                width: context.width,
+                                label: 'Entrar',
+                                onPressed: () {}
+                              )
+                            ),
+                            const Spacer(), //Joga tudo que vier depois para parte de baixo da tela
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                               const Text('Não possui uma conta?'),
+                               TextButton(
+                                 onPressed: () {
+                                   Get.toNamed('/auth/register');
+                                 },
+                                 child: const Text(
+                                   'Cadastre-se',
+                                   style: VakinhaUi.textBold,
+                                 ),
+                               )
+                              ],
                             )
-                          ),
-                          const Spacer(), //Joga tudo que vier depois para parte de baixo da tela
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                             const Text('Não possui uma conta?'),
-                             TextButton(
-                               onPressed: () {
-                                 Get.toNamed('/auth/register');
-                               },
-                               child: const Text(
-                                 'Cadastre-se',
-                                 style: VakinhaUi.textBold,
-                               ),
-                             )
-                            ],
-                          )
-                        ],
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                )
+                  )
+                ),
               );
             },
           ),
